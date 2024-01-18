01488a9121a3976842ebee1225dcd3db958972c4
Doodles <moraes.eduardo@proton.me>
2024-01-18 02:19:58 -0300

    chore: improve data models for pizza, sauce, and toppings

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PizzaAPI.Models;
 
 public class Pizza
 {
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
     public string? Name { get; set; }
+
     public bool IsGlutenFree { get; set; }
+
     public Sauce? Sauce { get; set; }
+
     public ICollection<Topping>? Toppings { get; set; }
 }
