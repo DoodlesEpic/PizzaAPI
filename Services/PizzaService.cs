53be45333607dc011d9747837024b360fad1dd30
Doodles <moraes.eduardo@proton.me>
2024-01-18 18:41:06 -0300

    chore: remove PizzaService implementation

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -4,55 +4,17 @@ namespace PizzaAPI.Services;
 
 public class PizzaService
 {
-    static List<Pizza> Pizzas { get; }
-    static int nextId = 3;
+    PizzaService() { }
 
-    static PizzaService()
-    {
-        Pizzas =
-        [
-            new()
-            {
-                Id = 1,
-                Name = "Classic Italian",
-                IsGlutenFree = false
-            },
-            new()
-            {
-                Id = 2,
-                Name = "Veggie",
-                IsGlutenFree = true
-            }
-        ];
-    }
+    public List<Pizza> GetAll() => throw new NotImplementedException();
 
-    public List<Pizza> GetAll() => Pizzas;
+    public Pizza? Get(int id) => throw new NotImplementedException();
 
-    public Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+    public void Add(Pizza pizza) => throw new NotImplementedException();
 
-    public void Add(Pizza pizza)
-    {
-        pizza.Id = nextId++;
-        Pizzas.Add(pizza);
-    }
+    public void Delete(int Id) => throw new NotImplementedException();
 
-    public void Delete(int Id)
-    {
-        var pizza = Get(Id);
-        if (pizza is null)
-            return;
-
-        Pizzas.Remove(pizza);
-    }
-
-    public void Update(Pizza pizza)
-    {
-        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
-        if (index == -1)
-            return;
-
-        Pizzas[index] = pizza;
-    }
+    public void Update(Pizza pizza) => throw new NotImplementedException();
 
     public void AddTopping(int PizzaId, int ToppingId) => throw new NotImplementedException();
 
