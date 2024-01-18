31358848304c30b28980d1ab196c2c314e94bfb1
Doodles <moraes.eduardo@proton.me>
2024-01-18 01:30:28 -0300

    chore: add sauce and toppings to pizza model

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -3,6 +3,8 @@ namespace PizzaAPI.Models;
 public class Pizza
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string? Name { get; set; }
     public bool IsGlutenFree { get; set; }
+    public Sauce? Sauce { get; set; }
+    public ICollection<Topping>? Toppings { get; set; }
 }
