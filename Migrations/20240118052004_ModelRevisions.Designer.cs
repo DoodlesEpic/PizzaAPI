1458c0ba516585756baef567400f4fa4a6f035e1
Doodles <moraes.eduardo@proton.me>
2024-01-18 02:20:15 -0300

    chore: create ModelRevisions migration

--- Migrations/20240118052004_ModelRevisions.Designer.cs
+++ Migrations/20240118052004_ModelRevisions.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PizzaAPI.Data;
 
@@ -10,9 +11,11 @@ using PizzaAPI.Data;
 namespace PizzaAPI.Migrations
 {
     [DbContext(typeof(PizzaContext))]
-    partial class PizzaContextModelSnapshot : ModelSnapshot
+    [Migration("20240118052004_ModelRevisions")]
+    partial class ModelRevisions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
